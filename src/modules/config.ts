5c76f690a1b7e4b805d471ce046f05e73ad18562
luslus <1911898144@qq.com>
2021-11-24 22:13:06 +0800

    提交已经编写的文档

--- src/modules/config.ts
+++ src/modules/config.ts
@@ -2,4 +2,5 @@ export const OS = false
 export const EVENT = false
 export const WORK_THREAD = false
 
-export const SQL_GENERATOR= true
+export const SQL_GENERATOR= false
+export const THREAD = false
